b84aee40de708fada284d1b53567620e643a12d4
Gabriel Teles <gabrielfariateles@gmail.com>
2023-08-15 17:52:33 -0300

    🔒️ chore: route '/new' is only accessible by administrators now.

--- FrontEnd/src/routes/app.routes.jsx
+++ FrontEnd/src/routes/app.routes.jsx
@@ -1,5 +1,6 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
+import { useAuth } from "../hooks/auth";
 import { SearchProvider } from "../hooks/searchContext";
 
 import { Home } from "../pages/Home";
@@ -7,12 +8,18 @@ import { Details } from "../pages/Details";
 import { New } from "../pages/New";
 
 export function App() {
+  const { isAdmin } = useAuth();
+
+  function AdminRoute({ element: Component }) {
+    return isAdmin ? <Component /> : <Navigate to="/" />;
+  }
+
   return (
     <SearchProvider>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/details/:id" element={<Details />} />
-        <Route path="/new" element={<New />} />
+        {isAdmin && <Route path="/new" element={<New />} />}
       </Routes>
     </SearchProvider>
   );

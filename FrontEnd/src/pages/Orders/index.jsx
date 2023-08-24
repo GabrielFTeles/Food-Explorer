5dac59e5796505839316e46679a7634369a90ece
Gabriel Teles <gabrielfariateles@gmail.com>
2023-08-23 22:45:38 -0300

    ✅ Order page.

--- FrontEnd/src/pages/Orders/index.jsx
+++ FrontEnd/src/pages/Orders/index.jsx
@@ -1,52 +1,167 @@
+import { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { useCart } from '../../hooks/cart';
 
 import { Container } from './styles';
+import { Input } from '../../components/Input';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 import { Button } from '../../components/Button';
+import { BackButton } from '../../components/BackButton';
 import { ProductItem } from '../../components/ProductItem';
+
+import { CreditCard, CaretLeft } from '@phosphor-icons/react'
+import PixIcon from '../../assets/PixIcon.svg';
+import QrCode from '../../assets/QrCode.svg';
 
 export function Orders() {
   const { cart, removeFromCart, getCartTotalPrice } = useCart();
+  const isDesktop = useMediaQuery({ minWidth: 1024 });
+
+  const [proceededToPayment, setProceededToPayment] = useState(false);
+  const [paymentMethod, setPaymentMethod] = useState('pix');
+
   const totalPrice = getCartTotalPrice();
+
+  function handleProceedToPayment() {
+    setProceededToPayment(true);
+  }
+
+  function handleMethodChange(method) {
+    setPaymentMethod(method);
+  }
 
   return (
     <Container>
       <Header />
 
        <main>
-        <h1>Meu pedido</h1>
+        {
+          (!proceededToPayment || isDesktop) && (
+            <div className="order">
+              {
+                !isDesktop && (
+                  <BackButton />
+                )
+              }
 
-        <ul>
-          {
-            cart.map(product => (
-              <ProductItem
-                key={product.id}
-                image={product.image}
-                title={product.title}
-                price={product.price}
-                quantity={product.quantity}
-                buttonText="Remover"
-                onClick={() => {removeFromCart(product.id)}}
-              />
-            ))
-          }
-        </ul>
+              <h1>Meu pedido</h1>
 
-        <span>Total: {" "}
-          {
-            new Intl.NumberFormat('pt-BR', {
-              style: 'currency',
-              currency: 'BRL'
-            }).format(totalPrice)
-          }
-        </span>
+              <ul>
+                {
+                  cart.map(product => (
+                    <ProductItem
+                      key={product.id}
+                      image={product.image}
+                      title={product.title}
+                      price={product.price}
+                      quantity={product.quantity}
+                      buttonText="Remover"
+                      onClick={() => {removeFromCart(product.id)}}
+                    />
+                  ))
+                }
+              </ul>
+
+              <span>Total: {" "}
+                {
+                  new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                  }).format(totalPrice)
+                }
+              </span>
+
+              {
+                (cart.length > 0 && !isDesktop) && (
+                  <Button 
+                    className="proceed-btn"
+                    onClick={handleProceedToPayment}
+                    title="Avançar"
+                  />
+                )
+              }
+            </div>
+          )
+        }
 
         {
-          cart.length > 0 && (
-            <Button 
-              title="Avançar"
-            />
+          (proceededToPayment || isDesktop) && (
+            <div className="payment">
+              {
+                !isDesktop && (
+                  <button className="back-btn" onClick={() => setProceededToPayment(false)}>
+                    <CaretLeft size={20} />
+                    Voltar
+                  </button>
+                )
+              }
+
+              <h2>Pagamento</h2>
+
+              <div className="payment-container">
+                <div className="payment-methods">
+                  <button 
+                    className={paymentMethod === "pix" ? "active" : ""}
+                    onClick={() => handleMethodChange('pix')}
+                  >
+                    <img src={PixIcon} alt="Pix" />
+                    <span>PIX</span>
+                  </button>
+
+                  <button
+                    className={paymentMethod === "credit" ? "active" : ""}
+                    onClick={() => handleMethodChange('credit')}
+                  >
+                    <CreditCard size={30} />
+                    <span>Crédito</span>
+                  </button>
+                </div>
+
+                <div className="payment-info">
+                  {
+                    paymentMethod === "pix" && (
+                      <div className="pix">
+                        <img src={QrCode} alt="QR Code" />
+                      </div>
+                    )
+                  }
+
+                  {
+                    paymentMethod === "credit" && (
+                      <div className="credit-card">
+                        <Input
+                          type="text"
+                          id="card-number"
+                          label="Número do cartão"
+                          placeholder="0000 0000 0000 0000"
+                        />
+
+                        <div>
+                          <Input
+                            type="text"
+                            id="card-expiration"
+                            label="Validade"
+                            placeholder="04/25"
+                          />
+
+                          <Input
+                            type="text"
+                            id="card-cvc"
+                            label="CVC"
+                            placeholder="000"
+                          />
+                        </div>
+
+                        <Button
+                          title="Finalizar pagamento"
+                        />
+                      </div>
+                    )
+                  }
+                </div>
+              </div>
+            </div>
           )
         }
        </main>
--- FrontEnd/src/pages/Orders/styles.js
+++ FrontEnd/src/pages/Orders/styles.js
@@ -8,34 +8,128 @@ export const Container = styled.div`
   > main {
     display: flex;
     flex-direction: column;
-    padding-block: 5.6rem;
+    padding-block: 3.6rem;
 
-    > h1 {
+    h1 {
+      margin-top: 3rem;
+    }
+
+    h1,
+    h2 {
       color: ${({ theme }) => theme.COLORS.LIGHT_300};
       font-size: 3.2rem;
       font-weight: 500;
       line-height: 140%;
 
-      margin-bottom: 2.7rem;
+      margin-bottom: 2rem;
     }
 
-    ul {
+    .order {
       display: flex;
       flex-direction: column;
+      width: 100%;
+
+      ul {
+        display: flex;
+        flex-direction: column;
+      }
+
+      > span {
+        margin-top: 1.6rem;
+        color: ${({ theme }) => theme.COLORS.LIGHT_300};
+        font-size: 2rem;
+        font-weight: 500;
+        line-height: 160%;
+      }
+
+      .proceed-btn {
+        max-width: 50%;
+        align-self: flex-end;
+        margin-top: 4.7rem;
+      }
     }
 
-    > span {
-      margin-top: 1.6rem;
+    .back-btn {
+      display: flex;
+      align-items: center;
+      background: transparent;
       color: ${({ theme }) => theme.COLORS.LIGHT_300};
       font-size: 2rem;
       font-weight: 500;
       line-height: 160%;
+      margin-bottom: 1.7rem;
     }
 
-    > button {
-      max-width: 50%;
-      align-self: flex-end;
-      margin-top: 4.7rem;
+    .payment {
+      width: 100%;
+    }
+
+    .payment-container {
+      border: 1px solid ${({ theme }) => theme.COLORS.LIGHT_600};
+      border-radius: 8px 8px 5px 5px;
+      overflow: hidden;
+
+      .payment-methods {
+        display: flex;
+
+        button {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          gap: 0.8rem;
+          font-family: Roboto;
+          height: 8rem;
+          background: transparent;
+          width: 100%;
+
+          &.active {
+            background: ${({ theme }) => theme.COLORS.DARK_800};
+          }
+
+          & + button {
+            border-left: 1px solid ${({ theme }) => theme.COLORS.LIGHT_600};
+          }
+        }
+      }
+
+      .payment-info {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-top: 1px solid ${({ theme }) => theme.COLORS.LIGHT_600};
+        padding: 3.2rem 2.6rem;
+
+        .credit-card {
+          display: flex;
+          flex-direction: column;
+          gap: 3.7rem;
+
+          div {
+            display: flex;
+            gap: 1.7rem;
+          }
+
+          .input-wrap {
+            border: 1px solid ${({ theme }) => theme.COLORS.LIGHT_100};
+            background: transparent;
+          }
+        }
+      }
+    }
+
+    @media screen and (min-width: 1024px) {
+      width: min(100%, 112rem);
+      flex-direction: row;
+      gap: 7rem;
+
+      .order ul {
+        max-height: 41.6rem;
+        overflow-y: auto;
+      }
+    }
+
+    @media screen {
+      
     }
   }
 `;
